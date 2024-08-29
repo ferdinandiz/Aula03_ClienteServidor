49481f90c0713a0f7fa1998ca4a896cda87db78f
Fernando Gonçalves Abadia <80607754+ferdinandiz@users.noreply.github.com>
2024-08-29 19:18:49 -0300

    Add files via upload

---
+++ src/main/java/org/example/model/Alunos.java
@@ -0,0 +1,16 @@
+package org.example.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Alunos {
+    Long matricula;
+    String nome;
+    String telefone;
+    boolean maioridade;
+    Cursos curso;
+    String sexo;
+}
